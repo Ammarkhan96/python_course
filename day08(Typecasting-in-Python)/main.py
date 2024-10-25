4351a6a6be04f6c72aa0ac5106117237e5dd890e
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-10-25 08:33:00 +0500

    python-course

---
+++ day08(Typecasting-in-Python)/main.py
@@ -0,0 +1,18 @@
+# a = "1"
+# a = 1
+# b = "2"
+# b = 2
+# print(a+b)
+
+
+# To convert string into number
+# a = '1'
+# b = '2'
+# print(int(a)+int(b))
+
+
+#Implicit Conversion:
+
+c = 1.9
+d = 8
+print(c+d)
