a8ef663aa48b5dec44264b845aec25289e6cb753
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-10-29 08:51:39 +0500

    python-course

---
+++ day10(strings)/main.py
@@ -0,0 +1,27 @@
+# name = "Ammar"
+# print("Hello", name)
+
+ #apple = 'He said, "I want to eat an apple". '
+# print(apple)
+
+name = "Ammar"
+friend = "Rohan"
+anotherFriend = 'Lovish'
+apple = '''He said, 
+Hi Ammar
+hey I am good
+"I want to eat an apple'''
+print("Hello, " + name)
+# print(apple)
+
+
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+
+
+print("Lets use a for Loop\n")
+for character in name:
+    print(character)
