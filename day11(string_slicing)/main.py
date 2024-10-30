d8ad8fde1e216eca0fb87904deed13d27a3c28f9
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-10-30 12:02:07 +0500

    python-course

---
+++ day11(string_slicing)/main.py
@@ -0,0 +1,26 @@
+# names = "Ammar, Khan"
+# print(len(names))
+
+# fruit = "Mango"
+# len1 = len(fruit)
+# print("Mango is a", len1, "letter word.")
+
+#slicing
+
+fruit = 'Mango'
+mangoLen = len(fruit)
+print(mangoLen)
+print(fruit[0: 4])
+print(fruit[1: 4])
+print(fruit[: 4])
+print(fruit[:])
+
+#negative slicing
+
+print(fruit[0: -3])
+print(fruit[-3 : -1])
+
+#Quick Exercise:
+
+nm = "Ammar"
+print(nm[-4:-2])
