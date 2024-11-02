92b4de764d0aa6174ee230085bd17180764b7158
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-11-02 10:54:44 +0500

    python-course

---
+++ day13(if_else_conditional)/if-else.py
@@ -0,0 +1,18 @@
+# applePrice = 140
+# budget = 110
+
+# if(applePrice <= budget):
+#     print("Alexa, add 1 kg Apple to the cart.")
+# else:
+#     print("Alexa , do not add Apples to the cart.")
+
+
+applePrice = 10
+budget = 200
+
+if(budget - applePrice > 50):
+    print("Alexa, add 1 kg Apple to the cart.")
+elif(budget - applePrice > 20):
+    print("Its okay you can buy")
+else:
+    print("Alexa , do not add Apples to the cart.")
---
+++ day13(if_else_conditional)/main.py
@@ -0,0 +1,17 @@
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+
+# Conditional Operator
+# >, <, >=, <=, ==, !=
+
+# print(a>18)
+# print(a<18)
+# print(a>=18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+
+if a > 18:
+    print("You can drive")
+else:
+    print("You cannot drive")
