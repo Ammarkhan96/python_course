6c3fa7ff59b82a1cb0ff467ea7ab1e05ab86847c
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-11-02 11:00:18 +0500

    python-course

--- day13(if_else_conditional)/nested.py
+++ day13(if_else_conditional)/nested.py
@@ -10,3 +10,17 @@ elif(num > 0):
         print("Number is greater than 20")
 else:
     print("Number is zero")
+
+
+num = 65
+if(num < 2):
+    print("Number is negative.")
+elif(num > 4):
+    if(num <= 20):
+        print("Number is between 1-10")
+    elif(num > 14 and num <= 30):
+        print("Number is between 11-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is zero")
