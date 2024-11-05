ef8d4855e1a6438d4e94d6640e086030e2d65d95
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-11-05 10:13:18 +0500

    python-course

---
+++ day16(for_loops)/main.py
@@ -0,0 +1,39 @@
+# name = 'Abdullah'
+# for i in name:
+#     print(i)
+
+
+# name = 'Abdullah'
+# for i in name:
+#     print(i)
+#     if(i == "b"):
+#         print("This is something special!")
+
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+
+
+
+# colors = ["Red", "Green", "Blue", "Yellow"]
+# for color in colors:
+#     print(color)
+#     for i in color:
+#         print(i)
+
+
+# for k in range(5):
+#     print(k)
+#     print(k + 1)
+
+
+# for k in range(1, 9):
+#     print(k)
+
+# for k in range(1, 20001):
+#     print(k)
+
+
+# for k in range(1, 12, 3):
+#     print(k)
