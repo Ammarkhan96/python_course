972a9c8454408bca829c5722c1a356d816a0e746
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-11-08 10:21:10 +0500

    python-course

---
+++ day17(while_loops)/main.py
@@ -0,0 +1,33 @@
+# i = 0
+# while(i<3):
+#     print(i)
+#     i = i + 1
+
+# i = 0
+# while(i<=3):
+#     print(i)
+#     i = i + 1
+
+#     print("Done with the loop")
+
+
+# i = int(input("Enter the numner: "))
+# while(i<=38):
+#     i = int(input("Enter the numner: "))
+#     print(i)
+
+#     print("Done with the loop")
+
+
+# Decrementing While Loop
+# count = 5
+# while(count > 0):
+#     print(count)
+#     count = count -1
+
+count = 5
+while(count > 0):
+    print(count)
+    count = count -1
+else:
+    print("I am inside else")
