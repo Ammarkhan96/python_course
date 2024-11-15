e0e2777016a0ab8f8f2ee5b64934abd68b9c35ee
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-11-15 08:43:47 +0500

    python-course

---
+++ day22(list_methods)/main.py
@@ -0,0 +1,64 @@
+# append:
+
+# l = [1, 2, 4, 6]
+# print(l)
+# l.append(7)
+# print(l)
+
+
+# sort:
+
+# l = [11, 45, 1, 2, 4, 6]
+# l.sort()
+# print(l)
+
+# l = [11, 45, 1, 2, 4, 6]
+# l.sort(reverse=True)
+# print(l)
+
+# reverse
+
+# l = [100, 32, 7, 8, 31, 21, 11, 9, 2]
+# l.reverse()
+# print(l)
+
+
+# index
+
+# l = [100, 32, 7, 8, 31, 21, 11, 9, 2]
+# print(l.index(11))
+
+
+# count:
+
+# l = [1, 2, 4, 9, 2, 54, 12, 20, 2]
+# print(l.count(2))
+
+# copy
+
+# l = [1, 2, 4, 9, 2, 54, 12, 20, 2]
+# newList = l.copy()
+# print(l)
+# print(newList)
+
+# insert
+
+# l = [1, 3, 6, 13, 16, 19, 24, 33, 56, 44]
+# l.insert(4, 67)
+# print(l)
+
+
+# extent
+
+# l = [1, 3, 6, 13, 16, 19, 24, 33, 56, 44]
+
+# m = [986, 1000, 1100, 1200]
+# l.extend(m)
+# print(l)
+
+# concatenating two list:
+
+l = [1, 3, 6, 13, 16, 19, 24, 33, 56, 44]
+m = [986, 1000, 1100, 1200]
+k = l + m
+print(k)
