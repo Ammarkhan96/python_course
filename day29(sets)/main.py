853435e9c33eb019f32f592671d549f9065d9ad5
Ammarkhan96 <ammarkhaan96@gmail.com>
2024-11-25 08:33:46 +0500

    python-course

---
+++ day29(sets)/main.py
@@ -0,0 +1,18 @@
+# sets:
+
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+
+# ammar = {}
+# print(type(ammar))
+
+ammar = set()
+print(type(ammar))
+
+
+for value in info:
+    print(value)
